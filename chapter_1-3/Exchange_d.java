571fecfdfce90668c086deeb849de53c562db746
yanqing-yang <55191318+yanqing-yang@users.noreply.github.com>
2020-03-13 11:05:44 +0800

    Exchange

---
+++ chapter_1-3/Exchange_d.java
@@ -0,0 +1,10 @@
+package d_u;
+
+public class Exchange_d {
+	public static void main(String[] args) {
+		int Random=(int)(15*Math.random());
+		System.out.println("This ten number is :"+Random);
+		
+	}
+
+}
